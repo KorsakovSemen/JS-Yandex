79007a2a1e694c311e612fcd777c427cf39f8b7c
pako2 <deacle2009@mail.ru>
2020-06-15 10:38:43 +0700

    complete 3 week

--- 1 course/3 week/index.js
+++ 1 course/3 week/index.js
@@ -3,20 +3,80 @@
  * @returns {Object}
  */
 module.exports = function (date) {
+
     var newDate = new Date(date);
-    console.log(newDate.getFullYear())
+
     return {
+
         add : function(val, type){
-            newDate.setDate(newDate.getDate() + 2);
-            return this.newDate;
+
+            if(val < 0)
+                throw new TypeError('Отрицательное значение');
+
+            switch(type){
+                case 'years':
+                    newDate.setFullYear(newDate.getFullYear() + val);
+                    break;
+                case 'months':
+                    newDate.setMonth(newDate.getMonth() + val);
+                    break;
+                case 'days':
+                    newDate.setDate(newDate.getDate() + val);
+                    break;
+                case 'hours':
+                    newDate.setHours(newDate.getHours() + val);
+                    break;
+                case 'minutes':
+                    newDate.setMinutes(newDate.getMinutes() + val);
+                    break;
+                case 'seconds':
+                    newDate.setSeconds(newDate.getSeconds() + val);
+                    break;
+                default:
+                    throw new TypeError('Неизвестный тип')
+            }
+            
+            this.value = newDate.getFullYear() + '-' + '0' + (newDate.getMonth() + 1) + '-' + newDate.getDate() + ' ' + newDate.getHours() + ':' + ("00" + newDate.getMinutes()).slice(-2);
+            console.log(this.value);
+            return this;
+
         },
         
         subtract : function(val, type){
-            console.log(newDate.getFullYear())
-            newDate.setDate(date.getDate() - val);
-            console.log(newDate.getFullYear())
-            return this.newDate;
+
+            if(val < 0){
+                throw new TypeError('Отрицательное значение');
+            }
+
+                switch(type){
+                    case 'years':
+                        newDate.setFullYear(newDate.getFullYear() - val);
+                        break;
+                    case 'months':
+                        newDate.setMonth(newDate.getMonth() - val);
+                        break;
+                    case 'days':
+                        newDate.setDate(newDate.getDate() - val);
+                        break;
+                    case 'hours':
+                        newDate.setHours(newDate.getHours() - val);
+                        break;
+                    case 'minutes':
+                        newDate.setMinutes(newDate.getMinutes() - val);
+                        break;
+                    case 'seconds':
+                        newDate.setSeconds(newDate.getSeconds() - val);
+                        break;
+                    default:
+                        throw new TypeError('Неизвестный тип')
+                }
+                
+                this.value = newDate.getFullYear() + '-' + '0' + (newDate.getMonth() + 1) + '-' + newDate.getDate() + ' ' + newDate.getHours() + ':' + ("00" + newDate.getMinutes()).slice(-2);
+                console.log(this.value);
+                return this;
+
         }
 
     }
+
 };
